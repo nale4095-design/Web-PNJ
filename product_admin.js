98f0d5ec1c8139777824a68140deab7ae7a6b46f
nguyenngoctien081-rgb <nguyenngoctien081@gmail.com>
2025-12-25 00:10:01 +0700

    Add files via upload

---
+++ product_admin.js
@@ -0,0 +1,47 @@
+const API = "http://localhost:3000/product";
+
+// Load sản phẩm
+fetch(API)
+    .then(res => res.json())
+    .then(data => {
+        console.log("Dữ liệu trả về:", data);
+        render(data);
+    })
+    .catch(err => console.log("Lỗi API:", err));
+
+// Render bảng
+function render(list) {
+    const tbody = document.getElementById("productBody");
+    tbody.innerHTML = "";
+
+    list.forEach((item, index) => {
+        tbody.innerHTML += `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${item.id}</td>
+                <td>${item.name}</td>
+                <td><img src="${item.img}" alt=""></td>
+                <td>${item.price}</td>
+                <td>
+                    <button class="btn-edit" onclick="editProduct(${item.id})">Sửa</button>
+                    <button class="btn-delete" onclick="deleteProduct(${item.id})">Xóa</button>
+                </td>
+            </tr>
+        `;
+    });
+}
+
+// Xóa sản phẩm
+function deleteProduct(id) {
+    if (confirm("Bạn có chắc muốn xóa sản phẩm này?") === false) return;
+
+    fetch(`${API}/${id}`, {
+        method: "DELETE"
+    })
+    .then(() => location.reload());
+}
+
+// Sửa sản phẩm
+function editProduct(id) {
+    alert("Sửa sản phẩm ID: " + id + " (mình sẽ làm form cho bạn tiếp)");
+}
